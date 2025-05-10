7447007f4eded81d961c4e1b2ab54ad40fb71403
rubenglezortiz <61495670+rubenglezortiz@users.noreply.github.com>
2025-05-10 18:35:21 +0200

    fallito de seguridad

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <iostream>
+#include <cstring>
 
 int main() {
-    std::cout << "Hola, mundo!" << std::endl;
-    return 0;
+
+	char buffer[10];
+	const char* input = "Esta cadena es demasiado larga";
+	std::strcpy(buffer, input);  // <- Esto genera alerta
+	std::cout << "Contenido: " << buffer << std::endl;
+	return 0;
+
 }
